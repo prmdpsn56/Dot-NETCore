9cadf372681a8d21aaea8d5030c96cc6cd5a33f9
Paramdeep Singh <prmdpsn56@gmail.com>
2025-03-15 14:32:54 -0700

    Added packges from nuget

---
+++ Blogging/Models/Domain/Tag.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Blogging.Models.Domain;
+
+public class Tag
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string DisplayName { get; set; }
+
+    public ICollection<BlogPost> BlogPosts { get; set; }
+}
